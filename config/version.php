fb53822fe8c2fe8ae1b840cc1a02b526092177b8
root <haboorn@163.com>
2018-12-27 18:24:22 +0800

    first commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Reconciliation',
+
+    'major'     =>  '1',
+
+    'minor'     =>  '3',
+
+    'patch'     =>  '6',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '26-December-2018',
+
+    'time'      =>  '17:30',
+
+    'zone'      =>  'GMT +3',
+
+];
